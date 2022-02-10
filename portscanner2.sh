214df2cc2416fdca7a9cf3ebde42952b3a3ba907
johnbilski <98566106+johnbilski@users.noreply.github.com>
2022-02-10 15:38:22 -0500

    Create portscanner2.sh

---
+++ portscanner2.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+echo "Select your output file: "
+read outputfile 
+
+echo 
+
+host1=$1
+port1=$2 
+
+printf "Host(s): \t\Port(s)\n" | tee $outfile
+for end in {1..254}; do 
+	for host in $(echo $host1.$end); do 
+		for port in $(echo $port1); do
+			timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null && printf "$host \t\t$port\n" | tee -a $outputfile
+		done 
+	done 
+done
